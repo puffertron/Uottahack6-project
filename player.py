d99f7278656d12e32e8265d95b5ebd8ed48a95e6
Ciaran-McDJ <ciaranmcdj@gmail.com>
2024-03-02 01:28:36 -0500

    added player.py and state.py

---
+++ player.py
@@ -0,0 +1,5 @@
+# Player holds info about the player like its audio clips and last few stances
+
+class Player():
+    def __init__(self) -> None:
+        pass
--- state.py
+++ state.py
@@ -1,1 +1,5 @@
-# Holds state of game. Is imported by most other files, does not import others
+# Holds state of game. Is imported by most other files, does not import other
+from player import Player
+
+player1:Player = None
+player2:Player = None
