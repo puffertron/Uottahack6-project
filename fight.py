bc37792758ac721c81e90fcbf0b097c10db74f31
Camila Restrepo <restrepoached@gmail.com>
2024-03-02 01:55:46 -0500

    not done gamemode, pushing so it can be referenced

---
+++ fight.py
@@ -0,0 +1,8 @@
+class Fight:
+    """Holds current gamemode and does the logic on the stances to find out what happens"""
+    gamemode: int
+
+    # Gamemode 1: Dance battle
+    @classmethod
+    def danceBattle(cls, stance-1: list[str], stance-2: list[str]):
+
