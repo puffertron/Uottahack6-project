4863d3bfa358aacfdbebd0c58d9f697b2e6d402a
Ciaran-McDJ <ciaranmcdj@gmail.com>
2024-03-02 01:51:20 -0500

    changed state to be class and added barebones of setup

---
+++ main.py
@@ -0,0 +1,19 @@
+from setup import setup
+
+def main():
+    
+    setup()
+
+    # TODO - set up time stuff
+    
+    # TODO - get input
+
+    # TODO - send input to Fight function
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- state.py
+++ state.py
@@ -1,5 +1,9 @@
 # Holds state of game. Is imported by most other files, does not import other
 from player import Player
 
-player1:Player = None
-player2:Player = None
+
+class State:
+    """State holds the changing state of the game, it is set up in setup then used and changed by other files"""
+    
+    player1:Player = None
+    player2:Player = None
