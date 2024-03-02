ed81a60e140c008f6c74be3a8b368df22d811def
Ciaran-McDJ <ciaranmcdj@gmail.com>
2024-03-02 02:36:01 -0500

    Adding time stuff to main

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 # Holds config values so they are all in one place and easy to change
 
 
+# Visual Settings
+windowWidth = 100 #pxl?
+windowHeight = 100 #pxl?
+
+
+
+# Audio
+
+beatLength = 1000 #ms
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import pygame as pg
 from setup import setup
+import config
+
 
 def main():
-    
+
+    pg.init()
+    screen = pg.display.set_mode((config.windowWidth, config.windowHeight))
+    # clock = pg.time.Clock()   - this is not actually used
+    running = True
     setup()
 
-    # TODO - set up time stuff
-    
-    # TODO - get input
 
-    # TODO - send input to Fight function
+    while running:
+
+        time_since_last_beat = pg.time.get_ticks() % config.beatLength
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.KEYDOWN:
+                if event.unicode == "a":
+                    print(pg.time.get_ticks())
+        
+        # TODO - get input
+
+        # TODO - send input to Fight function
+
+    
+    
 
 
 
