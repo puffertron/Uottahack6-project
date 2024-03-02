7232f52ea54422cfe53146894dea306c4f8fe61f
Camila Restrepo <restrepoached@gmail.com>
2024-03-02 02:29:54 -0500

    updates to fight, state, and player to get stuff ready

--- fight.py
+++ fight.py
@@ -1,8 +1,17 @@
+from State import State.player0 as player0, State.player1 as player1
+
 class Fight:
     """Holds current gamemode and does the logic on the stances to find out what happens"""
     gamemode: int
 
     # Gamemode 1: Dance battle
+    attacker = 0
+    attack_beat = True
     @classmethod
-    def danceBattle(cls, stance-1: list[str], stance-2: list[str]):
+    def danceBattle(cls, stance0: list[str], stance1: list[str]):
+        player0.addStance(stance0) # TODO rename to actual
+        player1.addStance(stance1) # TODO rename to actual
 
+        # TODO add the logic
+
+        cls.attack_beat = not cls.attack_beat
--- state.py
+++ state.py
@@ -4,6 +4,6 @@ from player import Player
 
 class State:
     """State holds the changing state of the game, it is set up in setup then used and changed by other files"""
-    
-    player1:Player = None
-    player2:Player = None
+    player0: Player = None
+    player1: Player = None
+    players = [player0,player1]
