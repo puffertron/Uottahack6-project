b68e84e60a9c8bfe85d96f8d1d67a8e1f2be4766
Ciaran-McDJ <ciaranmcdj@gmail.com>
2024-03-02 20:22:16 -0500

    Updated sound file constant names

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ def setup():
     State.players = [State.player0,State.player1]
 
     # Add sounds to players
-    State.player0.player_note = [pg.mixer.Sound(config.ATTACK_SOUND_FILE)]
-    State.player1.player_note = [pg.mixer.Sound(config.ATTACK_SOUND_FILE)]
+    State.player0.player_note = [pg.mixer.Sound(config.P0NOTE_FILE)]
+    State.player1.player_note = [pg.mixer.Sound(config.P1NOTE_FILE)]
     State.player0.player_chord = [pg.mixer.Sound(config.P0CHORD_FILE)]
     State.player1.player_chord = [pg.mixer.Sound(config.P1CHORD_FILE)]
-    State.player0.dodge_sound = pg.mixer.Sound(config.DODGE_SOUND_FILE)
-    State.player1.dodge_sound = pg.mixer.Sound(config.DODGE_SOUND_FILE)
-    State.player0.hit_sound = pg.mixer.Sound(config.HIT_SOUND_FILE)
-    State.player1.hit_sound = pg.mixer.Sound(config.HIT_SOUND_FILE)
-    State.player0.parry_sound = pg.mixer.Sound(config.PARRY_SOUND_FILE)
-    State.player1.parry_sound = pg.mixer.Sound(config.PARRY_SOUND_FILE)
-    State.player0.fumble_sound = pg.mixer.Sound(config.FUMBLE_SOUND_FILE)
-    State.player1.fumble_sound = pg.mixer.Sound(config.FUMBLE_SOUND_FILE)
+    State.player0.dodge_sound = pg.mixer.Sound(config.P0DODGE_SOUND_FILE)
+    State.player1.dodge_sound = pg.mixer.Sound(config.P1DODGE_SOUND_FILE)
+    State.player0.hit_sound = pg.mixer.Sound(config.P0HIT_SOUND_FILE)
+    State.player1.hit_sound = pg.mixer.Sound(config.P1HIT_SOUND_FILE)
+    State.player0.parry_sound = pg.mixer.Sound(config.P0PARRY_SOUND_FILE)
+    State.player1.parry_sound = pg.mixer.Sound(config.P1PARRY_SOUND_FILE)
+    State.player0.fumble_sound = pg.mixer.Sound(config.P0FUMBLE_SOUND_FILE)
+    State.player1.fumble_sound = pg.mixer.Sound(config.P1FUMBLE_SOUND_FILE)
 
     # Set channel volumes
     audio.player0_voice.set_volume(0.25)
